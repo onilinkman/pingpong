1936eaae99ed507683ca493eb144cbd527b67909
Christian Marban Callisaya <marbanchristian@gmail.com>
2020-03-19 21:16:54 -0400

    Create barraPong.gd

---
+++ barraPong.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+var velocidad=340
+var posOrg
+
+func _ready():
+	posOrg=position
+	
+
+func _physics_process(delta):
+	var posicion=Vector2()
+	var nombre=name
+	var boolArriba=Input.is_action_pressed(name+"Up")
+	var boolAbajo=Input.is_action_pressed(name+"Down")
+	var boolPot=Input.is_action_pressed(name+"Pot")
+	if boolArriba:
+		posicion+=Vector2(0,-1)
+	if boolAbajo:
+		posicion+=Vector2(0,1)
+	if boolPot:
+		velocidad=340*2
+	else:
+		velocidad=340
+	
+	move_and_slide(posicion*velocidad,Vector2(0,-1))
+
+func reiniciarPosicion():
+	position=posOrg
+
+
